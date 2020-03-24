443405bf47a8a6ff75f98d5ff45edfacfbca521d
unknown <pongpisut.kj@gmail.com>
2020-03-24 13:29:00 +0700

    Updates

---
+++ precache-manifest.cb2b41d335fa08e906fecfe1b418acde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51180a05d2aa33fc9442bc82e88a0f32",
+    "url": "/lab-8.io/index.html"
+  },
+  {
+    "revision": "751ed7e442b2797cce51",
+    "url": "/lab-8.io/static/css/main.4851e9fc.chunk.css"
+  },
+  {
+    "revision": "c2acf3f40b2f27cabf32",
+    "url": "/lab-8.io/static/js/2.da4bcfae.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/lab-8.io/static/js/2.da4bcfae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "751ed7e442b2797cce51",
+    "url": "/lab-8.io/static/js/main.e116b9e6.chunk.js"
+  },
+  {
+    "revision": "8ad97c2759d673c805b4",
+    "url": "/lab-8.io/static/js/runtime-main.12779e25.js"
+  }
+]);
